0757e60924153350803a190c0e25dce546223334
kritika3389 <kritika.3389@gmail.com>
2026-02-18 10:28:52 +0530

    [keertanaMudduluri] UC1: Application Entry & Welcome Message

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,6 +1,7 @@
 public class PalindroneCheckerApp {
     public static void main(String[] args){
-        System.out.println("welcome to palindrone checker app ");
+        System.out.println("welcome to palindrone checker Management System");
+        System.out.println("version 1.0");
+        System.out.println("System initialised successfully.");
     }
 }
-src/PalindroneCheckerApp.java
