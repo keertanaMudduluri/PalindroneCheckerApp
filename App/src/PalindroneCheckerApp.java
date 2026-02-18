2ac46af39fc6ff78c73322e8bd306b6a172687ba
kritika3389 <kritika.3389@gmail.com>
2026-02-18 10:22:38 +0530

    [Name] Added usecaseName

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindroneCheckerApp {
+    public static void main(String[] args){
+        System.out.println("welcome to palindrone checker app ");
+    }
+}
+src/PalindroneCheckerApp.java
